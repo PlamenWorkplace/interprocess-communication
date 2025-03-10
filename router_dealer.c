b03229f7e7276df56d3d448f09856666cc261293
Plamen <plamen@DESKTOP-CC7Q6SO>
2025-03-10 20:48:30 +0200

    Make req queue non-blocking

--- router_dealer.c
+++ router_dealer.c
@@ -131,7 +131,7 @@ void open_mqs(mqd_t *client2dealer_queue, mqd_t *dealer2worker1_queue, mqd_t *de
   // Open queues
   struct mq_attr attr = { .mq_maxmsg = MQ_MAX_MESSAGES, .mq_msgsize = sizeof(MQ_MESSAGE) };
 
-  *client2dealer_queue = mq_open(client2dealer_name, O_RDONLY | O_CREAT | O_EXCL, 0600, &attr);
+  *client2dealer_queue = mq_open(client2dealer_name, O_RDONLY | O_CREAT | O_EXCL | O_NONBLOCK, 0600, &attr);
   validate_mq(*client2dealer_queue, client2dealer_name);
   *dealer2worker1_queue = mq_open(dealer2worker1_name, O_WRONLY | O_CREAT | O_EXCL, 0600, &attr);
   validate_mq(*dealer2worker1_queue, dealer2worker1_name);
